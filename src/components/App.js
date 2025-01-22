8f0ad5d78df0a71d6a125059aba3e3d31b49c341
shivdasbhande05 <shivdasbhande@gmail.com>
2025-01-22 21:03:45 +0530

    Button Added

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [value , setCount] = useState(0);
+  const onAdd = () => {
+    setCount(value + 1);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
-        <button onClick={() => setCount((count) => count + 1)}>
-        </button>
-        <p>Button clicked {count} times</p>
+        <p>Button clicked {value} times.</p>
+
+        <button onClick={onAdd}>Click Me</button>
+        
     </div>
   )
 }
