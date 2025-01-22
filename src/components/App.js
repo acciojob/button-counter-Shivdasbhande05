b14b9e520d554401dd8442372a9854cca0c65881
shivdasbhande05 <shivdasbhande@gmail.com>
2025-01-22 21:16:05 +0530

    Count Updated

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,16 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const [value , setCount] = useState(0);
+  const [count , setCount] = useState(0);
   const onAdd = () => {
-    setCount(value + 1);
+    setCount(count + 1);
   }
   return (
     <div>
         {/* Do not remove the main div */}
-        <p>Button clicked {value} times.</p>
+        <p>Button clicked {count} times.</p>
 
-        <button onClick={onAdd}>Click Me</button>
+        <button onClick = {onAdd} >Click Me</button>
         
     </div>
   )
